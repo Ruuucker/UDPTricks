254147b15374c07724d4f0d2be9a470b01f072f5
Ruuucker <russ22223@gmail.com>
2020-12-04 09:16:59 -0500

    sdf

---
+++ setssiptest.js
@@ -0,0 +1,1 @@
+console.log('INVITE sip:root@192.168.221.134 SIP/2.0\n\rVia: SIP/2.0/UDP pc33.atlanta.com;branch=z9hG4bKkjshdyff\n\rTo: Bob <sip:root@192.168.1.205>\n\rFrom: Alice <sip:root@192.168.1.205>;tag=88sja8x\n\rMax-Forwards: 70\n\rCall-ID: 987asjd97y7atg\n\rCSeq: 986759 INVITE');
